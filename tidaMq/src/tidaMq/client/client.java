6b2655198b08d3f4dea8cd745a574fc3486e056a
buidat <dinhtuan95qh@gmail.com>
2022-04-22 17:41:20 +0700

    add multi client

---
+++ tidaMq/src/tidaMq/client/client.java
@@ -0,0 +1,36 @@
+package tidaMq.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class client {
+	public final static String SERVER_IP = "127.0.0.1";
+    public final static int SERVER_PORT = 3000;
+ 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = null;
+        try {
+            socket = new Socket(SERVER_IP, SERVER_PORT); // Connect to server
+            System.out.println("Connected: " + socket);
+ 
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            int ch=0;
+            os.write(1);
+            ch = is.read() ;
+            Thread.sleep(2000);
+            os.write(2);
+            ch = is.read() ;
+            Thread.sleep(2000);
+        } catch (IOException ie) {
+            System.out.println("Can't connect to server");
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
